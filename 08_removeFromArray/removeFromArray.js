790f1fbb23635a35fb7f1393ecdf60dbe47903c0
Kyle Jhong <29828796+kylejhong@users.noreply.github.com>
2025-05-27 18:40:53 -0700

    Finished Exercises 1 through 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
 const removeFromArray = function() {
+    let newArr = arguments[0].slice();
+    
+    for (let i = 1; i < arguments.length; i++) {
+        newArr = newArr.filter((element) => (element !== arguments[i]));
+    }
+    return newArr;
 };
 
 // Do not edit below this line
